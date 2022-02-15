5ade5bb44a32eb745dcfb2c1318764f517157aea
Kiril Klein <kleinkiril94@gmail.com>
2022-02-15 16:17:00 +0100

    adding minor options

--- case_control.py
+++ case_control.py
@@ -27,7 +27,7 @@ results_dir = join(base_dir, 'results')
 population_size = 10000
 num_subset = 1000
 num_hidden_variables = 0
-true_OR = 1.5
+true_OR = 2
 random_state = None
 beta = np.array([-2,1])
 gamma0=-4.5
@@ -48,15 +48,15 @@ def simulate_population(_):
     # df = matching.match(df)
     OR_subs, OR_95CI_subs, OR_pval_subs = stats.compute_OR_CI_pval(df[df.subset==1], 
                                                print_=False)
-    #OR_all, OR_95CI_all, OR_pval_all = stats.compute_OR_CI_pval(df, 
-    #                                            print_=False)                                                
+    OR_all, OR_95CI_all, OR_pval_all = stats.compute_OR_CI_pval(df, 
+                                                print_=False)                                                
     width_95CI_subs = OR_95CI_subs[1]-OR_95CI_subs[0]
     #width_95CI_all = OR_95CI_all[1]-OR_95CI_all[0]
     z_val = stats.OR_z_test(df)
     if OR_95CI_subs[0]<= 1 and OR_95CI_subs[1]>=1:
         includes_1 = 1
     else: includes_1 = 0
-    return round(z_val,5), includes_1, round(width_95CI_subs,5)
+    return OR_all, round(z_val,5), includes_1, round(width_95CI_subs,5), OR_pval_subs
 
 
 @timing
@@ -67,9 +67,13 @@ def main(num_experiments, num_procs=8):
     print("ORs from different matching methods applied to a subset of the population are compared to the true OR (whole population)")
     print("We measure bias by looking at what fraction of the experiments includes an OR of 1 in 95% CI, given that OR!=1")
     with mp.Pool(num_procs) as pool:
-        metrics = pool.map(simulate_population, np.arange(num_experiments))
-    print(metrics)
+        metrics_ls = pool.map(simulate_population, np.arange(num_experiments))
+    with open(join(results_dir, 'case_control_OR1_5.txt'), 'w') as f:
+        f.write("OR_all z includes_1 width_CI_subs pval_subs\n")
+        for t in metrics_ls:
+            f.write(' '.join(str(s) for s in t) + '\n')
+
 if __name__ == '__main__':
-    main(20, 5)
+    main(100, 10)
 
 
